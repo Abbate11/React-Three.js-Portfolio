6b94b15923b29458908d741e993a5e02535cb4c8
Christian Abbate <Christian@myabbate.com>
2024-06-27 13:30:47 -0400

    closing to upload projects

--- React-Three.js-Portfolio/src/Components/Works.jsx
+++ React-Three.js-Portfolio/src/Components/Works.jsx
@@ -50,38 +50,26 @@ const ListItem = styled.li`
     color: transparent;
     -webkit-text-stroke: 1px #28282B;
     position: relative;
+    padding: 5px;
+    border: 1px solid transparent;
+    border-radius: 10px;
 
     &:hover {
-      transition: all 100ms ease-out;
-    transform: translateY(-2px) translateX(-1px);
-    text-shadow: 1px 1px 1px #838383,
-        1px 2px 1px #919191,
-        1px 3px 1px #919191,
-        1px 4px 1px #919191,
-        1px 5px 1px #919191,
-        1px 6px 1px #919191,
-        1px 7px 1px #919191,
-        1px 8px 1px #919191,
-        
-    1px 18px 6px rgba(16,16,16,0.4),
-    1px 22px 10px rgba(16,16,16,0.2),
-    1px 25px 35px rgba(16,16,16,0.2),
-    1px 30px 60px rgba(16,16,16,0.4);
+      border: 1px solid grey;
     }
 
     &:active {
     transition: 100ms;
     transform: translateY(6px);
-    text-shadow: 1px 1px 1px #919191,
-        1px 2px 1px #919191,
-        1px 3px 1px #919191,
-        1px 4px 1px #919191,
-        1px 5px 1px #919191,
-        1px 6px 1px #919191,
-    1px 9px 3px rgba(16,16,16,0.4),
-    1px 11px 5px rgba(16,16,16,0.2),
-    1px 13px 17px rgba(16,16,16,0.2),
-    1px 15px 30px rgba(16,16,16,0.4);
+    box-shadow: 1px 1px 1px #919191 inset,
+        1px 2px 1px #919191 inset,
+        1px 3px 1px #919191 inset,
+        1px 4px 1px #919191 inset,
+    1px 18px 6px rgba(16,16,16,0.4) inset,
+    1px 22px 10px rgba(16,16,16,0.2) inset,
+    1px 25px 35px rgba(16,16,16,0.2) inset,
+    1px 30px 60px rgba(16,16,16,0.4) inset;
+    border: none;
   }
 `;
 
